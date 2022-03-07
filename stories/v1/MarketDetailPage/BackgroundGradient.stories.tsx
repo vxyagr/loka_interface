39e55c739280f1402663a0fab9b49b775e905cdc
winartan <hello.angkin@gmail.com>
2022-03-07 07:23:42 +0700

    remove stories

--- stories/v1/MarketDetailPage/BackgroundGradient.stories.tsx
+++
@@ -1,8 +0,0 @@
-import BackgroundGradient from "../../../components/v1/MarketDetailPage/BackgroundGradient";
-
-export default {
-    component: BackgroundGradient,
-    title: "Risedle V1/Market Detail Page/Background Gradient Component",
-};
-
-export const BackgroundGradientComponent = () => <BackgroundGradient />;
