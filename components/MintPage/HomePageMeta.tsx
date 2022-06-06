d6b5a57f0902c9ac68839cc1dbc5936cd9f2fdfb
vxyagr <hello.angkin@gmail.com>
2022-06-06 22:48:48 +0700

    meta

--- components/MintPage/HomePageMeta.tsx
+++ components/MintPage/HomePageMeta.tsx
@@ -15,19 +15,19 @@ const HomePageMeta: FunctionComponent<HomePageMetaProps> = ({}) => {
     return (
         <>
             {/* <!-- Facebook Meta Tags --> */}
-            <meta property="og:url" content="https://risedle.com/" />
+            <meta property="og:url" content="https://lokaverse.io/" />
             <meta property="og:type" content="website" />
-            <meta property="og:title" content="Risedle Protocol" />
-            <meta property="og:description" content="Invest, earn and build on the decentralized crypto leveraged ETFs market protocol" />
-            <meta property="og:image" content="https://risedle.com/og/Landing.png" />
+            <meta property="og:title" content="Loka Blockchain Mining with Renewable Energy" />
+            <meta property="og:description" content="A simple way to invest on a green energy based blockchain infrastructure, managed by professionals with transparency and reliability" />
+            <meta property="og:image" content="" />
 
             {/* <!-- Twitter Meta Tags --> */}
             <meta name="twitter:card" content="summary_large_image" />
             <meta property="twitter:domain" content="risedle.com" />
-            <meta property="twitter:url" content="https://risedle.com/" />
-            <meta name="twitter:title" content="Risedle Protocol" />
-            <meta name="twitter:description" content="Invest, earn and build on the decentralized crypto leveraged ETFs market protocol" />
-            <meta name="twitter:image" content="https://risedle.com/og/Landing.png" />
+            <meta property="twitter:url" content="https://lokaverse.io/" />
+            <meta name="twitter:title" content="Loka Blockchain Mining with Renewable Energy" />
+            <meta name="twitter:description" content="A simple way to invest on a green energy based blockchain infrastructure, managed by professionals with transparency and reliability" />
+            <meta name="twitter:image" content="" />
         </>
     );
 };
